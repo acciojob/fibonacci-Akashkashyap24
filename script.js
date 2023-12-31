3e74554a69a451cddf2ee4ab9bad95c190dda0c7
Akashkashyap24 <144315919+Akashkashyap24@users.noreply.github.com>
2023-12-31 13:33:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 function fibonacci(n) {
 // your code here
-	 if(n==1){
-		   return 0;
-	   }
-		if(n==2){
-			return 1;
-		}
+	 if(n<=1){
+		 return n;
+	 }
 	return fibonacci(n-1)+fibonacci(n-2);
 }
 module.exports = fibonacci;
